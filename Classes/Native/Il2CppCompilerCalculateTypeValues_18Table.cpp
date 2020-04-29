b0e64c4e55ad3125ea92bf61a919bf2064a6b9fa
LauraTkalcec <tkalcec.laura@gmail.com>
2020-04-29 15:42:24 +0200

    IV

---
+++ Classes/Native/Il2CppCompilerCalculateTypeValues_18Table.cpp
@@ -0,0 +1,102 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <cstring>
+#include <string.h>
+#include <stdio.h>
+#include <cmath>
+#include <limits>
+#include <assert.h>
+
+#include "class-internals.h"
+#include "codegen/il2cpp-codegen.h"
+#include "AssemblyU2DCSharp_Vuforia_MultiTargetBehaviour3504654311.h"
+#include "AssemblyU2DCSharp_Vuforia_ObjectTargetBehaviour3836044259.h"
+#include "AssemblyU2DCSharp_Vuforia_PropBehaviour966064926.h"
+#include "AssemblyU2DCSharp_Vuforia_ReconstructionBehaviour4009935945.h"
+#include "AssemblyU2DCSharp_Vuforia_ReconstructionFromTarget2111803406.h"
+#include "AssemblyU2DCSharp_Vuforia_SurfaceBehaviour2405314212.h"
+#include "AssemblyU2DCSharp_Vuforia_TextRecoBehaviour3400239837.h"
+#include "AssemblyU2DCSharp_Vuforia_TurnOffBehaviour3058161409.h"
+#include "AssemblyU2DCSharp_Vuforia_TurnOffWordBehaviour584991835.h"
+#include "AssemblyU2DCSharp_Vuforia_UserDefinedTargetBuildin4184040062.h"
+#include "AssemblyU2DCSharp_VRIntegrationHelper556656694.h"
+#include "AssemblyU2DCSharp_Vuforia_VideoBackgroundBehaviour3161817952.h"
+#include "AssemblyU2DCSharp_Vuforia_VirtualButtonBehaviour2515041812.h"
+#include "AssemblyU2DCSharp_Vuforia_VuMarkBehaviour2060629989.h"
+#include "AssemblyU2DCSharp_Vuforia_VuforiaBehaviour359035403.h"
+#include "AssemblyU2DCSharp_Vuforia_VuforiaConfiguration3823746026.h"
+#include "AssemblyU2DCSharp_Vuforia_VuforiaRuntimeInitializa1850075444.h"
+#include "AssemblyU2DCSharp_Vuforia_WireframeBehaviour2494532455.h"
+#include "AssemblyU2DCSharp_Vuforia_WireframeTrackableEventH1535150527.h"
+#include "AssemblyU2DCSharp_Vuforia_WordBehaviour3366478421.h"
+
+
+
+
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1800 = { sizeof (MultiTargetBehaviour_t3504654311), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1801 = { sizeof (ObjectTargetBehaviour_t3836044259), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1802 = { sizeof (PropBehaviour_t966064926), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1803 = { sizeof (ReconstructionBehaviour_t4009935945), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1804 = { sizeof (ReconstructionFromTargetBehaviour_t2111803406), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1805 = { sizeof (SurfaceBehaviour_t2405314212), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1806 = { sizeof (TextRecoBehaviour_t3400239837), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1807 = { sizeof (TurnOffBehaviour_t3058161409), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1808 = { sizeof (TurnOffWordBehaviour_t584991835), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1809 = { sizeof (UserDefinedTargetBuildingBehaviour_t4184040062), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1810 = { sizeof (VRIntegrationHelper_t556656694), -1, sizeof(VRIntegrationHelper_t556656694_StaticFields), 0 };
+extern const int32_t g_FieldOffsetTable1810[12] = 
+{
+	VRIntegrationHelper_t556656694_StaticFields::get_offset_of_mLeftCameraMatrixOriginal_2(),
+	VRIntegrationHelper_t556656694_StaticFields::get_offset_of_mRightCameraMatrixOriginal_3(),
+	VRIntegrationHelper_t556656694_StaticFields::get_offset_of_mLeftCamera_4(),
+	VRIntegrationHelper_t556656694_StaticFields::get_offset_of_mRightCamera_5(),
+	VRIntegrationHelper_t556656694_StaticFields::get_offset_of_mLeftExcessAreaBehaviour_6(),
+	VRIntegrationHelper_t556656694_StaticFields::get_offset_of_mRightExcessAreaBehaviour_7(),
+	VRIntegrationHelper_t556656694_StaticFields::get_offset_of_mLeftCameraPixelRect_8(),
+	VRIntegrationHelper_t556656694_StaticFields::get_offset_of_mRightCameraPixelRect_9(),
+	VRIntegrationHelper_t556656694_StaticFields::get_offset_of_mLeftCameraDataAcquired_10(),
+	VRIntegrationHelper_t556656694_StaticFields::get_offset_of_mRightCameraDataAcquired_11(),
+	VRIntegrationHelper_t556656694::get_offset_of_IsLeft_12(),
+	VRIntegrationHelper_t556656694::get_offset_of_TrackableParent_13(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1811 = { sizeof (VideoBackgroundBehaviour_t3161817952), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1812 = { sizeof (VirtualButtonBehaviour_t2515041812), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1813 = { sizeof (VuMarkBehaviour_t2060629989), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1814 = { sizeof (VuforiaBehaviour_t359035403), -1, sizeof(VuforiaBehaviour_t359035403_StaticFields), 0 };
+extern const int32_t g_FieldOffsetTable1814[1] = 
+{
+	VuforiaBehaviour_t359035403_StaticFields::get_offset_of_mVuforiaBehaviour_19(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1815 = { sizeof (VuforiaConfiguration_t3823746026), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1816 = { sizeof (VuforiaRuntimeInitialization_t1850075444), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1817 = { sizeof (WireframeBehaviour_t2494532455), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1817[4] = 
+{
+	WireframeBehaviour_t2494532455::get_offset_of_lineMaterial_2(),
+	WireframeBehaviour_t2494532455::get_offset_of_ShowLines_3(),
+	WireframeBehaviour_t2494532455::get_offset_of_LineColor_4(),
+	WireframeBehaviour_t2494532455::get_offset_of_mLineMaterial_5(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1818 = { sizeof (WireframeTrackableEventHandler_t1535150527), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1818[1] = 
+{
+	WireframeTrackableEventHandler_t1535150527::get_offset_of_mTrackableBehaviour_2(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1819 = { sizeof (WordBehaviour_t3366478421), -1, 0, 0 };
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
